b09afe43b2e6f3ed33ed128a2a5d626144e40a5c
Ramona Gospodinova <ramona.gospodinova@plana.solutions>
2023-02-27 13:50:58 +0200

    changes

--- Intro/JS Dom and Events - Exercises/08. Furniture/solution.js
+++ Intro/JS Dom and Events - Exercises/08. Furniture/solution.js
@@ -3,16 +3,18 @@ function solve() {
   let btnGenerate = document.getElementsByTagName('button')[0];
   let btnBuy = document.getElementsByTagName('button')[1];
 
-  let table = document.querySelectorAll('table tbody')[0];
+  let tableTbody = document.querySelectorAll('table tbody')[0];
 
   // btnGenerate
-  btnGenerate.addEventListener('click', btn => {
+  btnGenerate.addEventListener('click', () => {
 
-    //  [{"name": "Sofa", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 150, "decFactor": 1.2}]
+    //  [{"name": "Tablet", "img": "https://s12emagst.akamaized.net/products/16498/16497603/images/res_aec28fc5950c2a40e001ff99795e576b_200x200_l6m7.jpg", "price": 2000, "decFactor": 5.2}, {"name": "Vase", "img": "https://cdn.shoplightspeed.com/shops/606957/files/11398239/200x200x2/small-lady-vase.jpg", "price": 15, "decFactor": 10}]
     let area = document.getElementsByTagName('textarea')[0].value;
-    let object = JSON.parse(area)[0];
+    let objects = JSON.parse(area);
 
-    for (const property in object) {
+    for (let object of objects) {
+
+      // create a new row with td-s for each object (from JSON):
       let tr = document.createElement('tr');
       let tdImage = document.createElement('td');
       let imageObject = document.createElement('img');
@@ -20,49 +22,54 @@ function solve() {
       let tdName = document.createElement('td');
       let tdPrice = document.createElement('td');
       let tdDecFactor = document.createElement('td');
-  
-      if (property === 'name') {
-        let name = object[property];
-        tdName.textContent = name;
-        tr.appendChild(tdName);
-      } else if (property === 'img') {
-        let image = object[property];
-        imageObject.src = image;
-        tdImage.appendChild(imageObject);
-        tr.appendChild(tdImage);
-      } else if (property === 'price') {
-        let price = object[property];
-        tdPrice.textContent = price;
-        tr.appendChild(tdPrice);
-      }  else {
-        let decFactor = object[property];
-        tdDecFactor.textContent = decFactor;
-        tr.appendChild(tdDecFactor);
+      let tdCheckbox = document.createElement('td');
 
-        let tdCheckbox = document.createElement('input');
-        // Assigning the attributes
-        // to created checkbox
-        tdCheckbox.type = 'checkbox';
-        tr.appendChild(tdCheckbox);
+      for (let property in object) {
+        if (property === 'name') {
+          let name = object[property];
+          tdName.textContent = name;
+        } else if (property === 'img') {
+          let image = object[property];
+          imageObject.src = image;
+          tdImage.appendChild(imageObject);
+        } else if (property === 'price') {
+          let price = object[property];
+          tdPrice.textContent = price;
+        }  else {
+          let decFactor = object[property];
+          tdDecFactor.textContent = decFactor;
   
-        table.appendChild(tr);
+          let input = document.createElement('input');
+          // Assigning the attributes
+          // to created checkbox
+          input.type = 'checkbox';
+          tdCheckbox.appendChild(input)
+          
+          tr.appendChild(tdImage);
+          tr.appendChild(tdName);
+          tr.appendChild(tdPrice);
+          tr.appendChild(tdDecFactor);
+          tr.appendChild(tdCheckbox);
+    
+          tableTbody.appendChild(tr);
+        }
       }
     }
 
   })
 
   // btnBuy
-  btnBuy.addEventListener('click', item => {
+  btnBuy.addEventListener('click', () => {
     // get all checked input[type='checkbox'];
     let checkbox = document.getElementsByTagName('input');
-    let trCheckedCheckboxes = [...checkbox].filter(item => item.checked).map(item => item.parentNode);
+    let trNodes = [...checkbox].filter(item => item.checked).map(item => item.parentNode.parentNode);
     
     let furnitureNames = [];
     let totalPrice = 0;
     let factor = 0;
-    for (const tr of trCheckedCheckboxes) {
-        let children = tr.children;
-        for (let index = 0; index < children.length; index++) {
+    for (const tr of trNodes) {
+        let trChildren = tr.children;
+        for (let index = 0; index < trChildren; index++) {
             if (index == 1) {
               let furniture = children[index].textContent;
               furnitureNames.push(furniture);
